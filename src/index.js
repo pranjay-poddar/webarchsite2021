2a4057a49e9e5e891ce60ee057a99004e70a7e8d
Antara Gupta <71868258+Antara25-10@users.noreply.github.com>
2021-09-23 21:15:25 +0530

    Updated with necessary changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
-
+import Clients from './containers/Clients/Clients';
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -15,3 +15,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
