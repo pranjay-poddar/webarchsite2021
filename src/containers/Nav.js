1c2a54891226cf4bd3b0e3d942e1e6c27585e397
imcb <chiragwelcome@gmail.com>
2021-09-24 17:17:02 +0530

    second commit

--- src/containers/Nav.js
+++ src/containers/Nav.js
@@ -1,11 +1,20 @@
-import { Nav, Navbar } from "react-bootstrap";
-
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
-import "./App.css";
 
-function Menu() {
+import "./Nav.css";
+
+const Menu = () => {
   return (
     <div>
+      {/* const [show, setShow] = useState(false);  */}
+      {/* const showDropdown = () => {
+  setShow(!show);
+};
+const hideDropdown = () => {
+  setShow(false);
+}; */}
+
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">
@@ -21,19 +30,45 @@ function Menu() {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto"></Nav>
-            <Nav>
-              <Nav.Link href="#deets">Home</Nav.Link>
-              <Nav.Link href="#deets">About</Nav.Link>
-              <Nav.Link href="#deets">Services</Nav.Link>
-              <Nav.Link href="#deets">Client</Nav.Link>
-              <Nav.Link href="#deets">Contact</Nav.Link>
-              <Nav.Link href="#deets">Team</Nav.Link>
+            <Nav className="floating">
+              <Nav.Link className="element" href="#deets">
+                Home
+              </Nav.Link>
+              <Nav.Link className="element" href="#deets">
+                Projects
+              </Nav.Link>
+              <Nav.Link className="element" href="#deets">
+                Team
+              </Nav.Link>
+
+              <NavDropdown
+                title="Dropdown"
+                id="collasible-nav-dropdown"
+                // show={show}
+                // onMouseEnter={showDropdown}
+                // onMouseLeave={hideDropdown}
+              >
+                <NavDropdown.Item className="element" href="#action/3.1">
+                  Testimonials
+                </NavDropdown.Item>
+                <NavDropdown.Item className="element" href="#action/3.2">
+                  About
+                </NavDropdown.Item>
+                <NavDropdown.Item className="element" href="#action/3.3">
+                  Services
+                </NavDropdown.Item>
+                <NavDropdown.Item className="element" href="#action/3.4">
+                  Contact
+                </NavDropdown.Item>
+                {/* <NavDropdown.Divider /> */}
+                <NavDropdown.Item href="#action/3.5">Client</NavDropdown.Item>
+              </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
   );
-}
+};
 
 export default Menu;
