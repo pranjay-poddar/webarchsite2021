3d2f8f3205a8fab13a98a8a150c062ba852a270c
Mohammad Muazam <mm9084@srmist.edu.in>
2021-09-26 13:46:57 +0530

    Added Carousel

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,22 +1,22 @@
 import logo from "../files/logo.png";
 import { Link } from "react-router-dom";
-import "./Home.css"
-
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div className="Home">
-            <header className="Home-header">
-                <img src={logo} className="Home-logo" alt="logo" />
-                <p>Webarch Site 2021</p>
-                <a className="Home-link" href="https://webarchsrm.com" target="_blank" rel="noopener noreferrer">
-                    Main Branch
-                </a>
-                <Link to="/">Home</Link>
-                <Link to="/team">Team</Link>
-            </header>
-        </div>
-    );
-}
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <img src={logo} className="Home-logo" alt="logo" />
+        <p>Webarch Site 2021</p>
+        <a className="Home-link" href="https://webarchsrm.com" target="_blank" rel="noopener noreferrer">
+          Main Branch
+        </a>
+        <Link to="/">Home</Link>
+        <Link to="/team">Team</Link>
+        <Link to="/testimonials">Testimonials</Link>
+      </header>
+    </div>
+  );
+};
 
 export default Home;
